a3d18562e05501bbc2968eb7ec2c105cb1d44f50
Bartek Szczudło <bartolomaj@gmail.com>
2023-03-22 12:52:51 +0100

    Added GUI with JavaFX "Hello JavaFX!"

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import org.example.exceptions.PersistenceToFileException;
 import org.example.ui.text.TextUI;
 import org.example.util.Properties;
 
 import java.io.IOException;
 
-public class App {
+public class App extends Application {
 
     private static final TextUI textUI = new TextUI();
 
@@ -17,10 +21,23 @@ public class App {
         } catch (IOException e) {
             throw new PersistenceToFileException(Properties.DATA_DIRECTORY.toString(), "create", "directory");
         }
-        textUI.showSystemInfo();
-        textUI.showMainMenu();
+        Application.launch(args);
+//        textUI.showSystemInfo();
+//        textUI.showMainMenu();
 
 
+    }
+
+    public void start(Stage primaryStage){
+        String hotelName = Properties.HOTEL_NAME;
+        int systemVersion = Properties.SYSTEM_VERSION;
+
+        Label l = new Label("Hello JavaFX");
+        Scene scene = new Scene(l, 640, 480);
+        String title = String.format("System rezerwacji hotelu %s (%d)", hotelName, systemVersion);
+        primaryStage.setTitle(title);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 
